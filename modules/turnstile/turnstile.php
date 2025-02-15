d9c9a379cc80fe6fc71f496508dc4fd20f43e6f4
Takayuki Miyoshi <takayukister@gmail.com>
2025-02-15 17:13:18 +0900

    Add Turnstile as service

--- modules/turnstile/turnstile.php
+++ modules/turnstile/turnstile.php
@@ -1,1 +1,14 @@
 <?php
+
+include_once path_join( __DIR__, 'service.php' );
+
+
+add_action( 'wpcf7_init', 'wpcf7_turnstile_register_service', 35, 0 );
+
+function wpcf7_turnstile_register_service() {
+	$integration = WPCF7_Integration::get_instance();
+
+	$integration->add_service( 'turnstile',
+		WPCF7_Turnstile::get_instance()
+	);
+}
